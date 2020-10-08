04eb522e5c8163ad25234402eed91553e7cb18c2
Lukas <lukas.uhrig2000@gmail.com>
2020-10-08 11:09:35 +0200

    PID die sechste

--- PiApplication/ProjectUtopia/ProjectUtopia.py
+++ PiApplication/ProjectUtopia/ProjectUtopia.py
@@ -1,67 +1,18 @@
 import PID
+import pid_control
 import RPi.GPIO as GPIO
 import time
 
 #for x in range(0,10):
 
 
-P = 5
-I = 5
-D = 5
-Kp = 1
-Ki = 1
-Kd = 1
-
 PinMotorlinksvorwaerts = 1
 PinMotorrechtsvorwaerts = 2
 PinMotorlinksrueckwaerts = 3
 PinMotorrechtsrueckwaerts = 4
 
-
-def BasisKonfiguration():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PinMotorlinksvorwaerts, GPIO.OUT)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PinMotorrechtsvorwaerts, GPIO.OUT)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PinMotorlinksrueckwaerts, GPIO.OUT)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PinMotorrechtsrueckwaerts, GPIO.OUT)
-
-BasisKonfiguration()
-pid=PID.PID(P, I, D)
-
-def Konfiguration():
-    pid.setKp(Kp)
-    pid.setKi(Ki)
-    pid.setKd(Kd)
-
-def links_vorwaerts():
-    time.sleep(5)
-    GPIO.output(Pinmotorlinksvorwaerts,1)
-    time.sleep(5)
-    GPIO.output(Pinmotorlinksvorwaerts,0)
-
-def links_rueckwaerts():
-    time.sleep(5)
-    GPIO.output(Pinmotorlinksrueckwaerts,1)
-    time.sleep(5)
-    GPIO.output(Pinmotorlinksrueckwaerts,0)
-
-def rechts_vorwaerts():
-    time.sleep(5)
-    GPIO.output(Pinmotorrechtsvorwaerts,1)
-    time.sleep(5)
-    GPIO.output(Pinmotorrechtsvorwaerts,0)
-
-def rechts_rueckwaerts():
-    time.sleep(5)
-    GPIO.output(Pinmotorrechtssrueckwaerts,1)
-    time.sleep(5)
-    GPIO.output(Pinmotorrechtsrueckwaerts,0)
-
 while (1):
-    Konfiguration()
+    pid_control.pid_control.reglung();
     Eingang=pin1 # keinen Schimmer
     Bewegung=Eingang*2 # keinen Schimmer
 
