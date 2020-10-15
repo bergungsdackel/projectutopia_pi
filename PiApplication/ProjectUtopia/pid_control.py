555df4fad9ff403a979c2e5e7c564ee3fe0a1cfe
dominikguba <56926355+dominikguba@users.noreply.github.com>
2020-10-15 12:39:24 +0200

    Motor control

--- PiApplication/ProjectUtopia/pid_control.py
+++ PiApplication/ProjectUtopia/pid_control.py
@@ -67,37 +67,8 @@ class pid_control(object):
         PID.PID.pid(eingang)
         geregelterWert = PID.PID.Ausgang
     #########################################################################################
- 
-    def __init__(self, enPin: int, inForewardPin: int, inBackwardPin: int):
-        GPIO.setup(inForewardPin, GPIO.OUT)
-        self.inForewardPin = inForewardPin
-        GPIO.output(inForewardPin, False)
- 
-        self.inBackwardPin = inBackwardPin
-        GPIO.setup(inBackwardPin, GPIO.OUT)
-        GPIO.output(inBackwardPin, False)
- 
-        GPIO.setup(enPin, GPIO.OUT)
-        self.pwm = GPIO.PWM(enPin, 100)
-        self.pwm.start(0)
-        self.pwm.ChangeDutyCycle(0)
- 
-    def setSpeed(self, speed: int):
-        """
-            speed might be -3, -2, -1, 0, 1, 2, 3
-        """
-        force = min(3, abs(speed))
-        isForewards = speed > 0
-        if (isForewards):
-            GPIO.output(self.inBackwardPin, False)
-            GPIO.output(self.inForewardPin, True)
-        else:
-            GPIO.output(self.inForewardPin, False)
-            GPIO.output(self.inBackwardPin, force > 0)
-        if force > 0:
-            self.pwm.ChangeDutyCycle(70 + (10 * force))
-        else:
-            self.pwm.ChangeDutyCycle(0)
+
+   
 
 
  
