6ac1bfb64a09117fb72e26f96543fb23f00c98d6
Lukas <lukas.uhrig2000@gmail.com>
2020-12-18 12:00:08 +0100

    pid außer funktion gesetzt

--- PiApplication/ProjectUtopia/pid_control.py
+++ PiApplication/ProjectUtopia/pid_control.py
@@ -62,7 +62,9 @@ class pid_control(object):
 
        #print("Speedlinks %d" % self.speedlinks)
        #print("Speedrechts %d" % self.speedrechts)
-       motoranpassung = self.motoranpassung(x_rotation, speed, turn)
+       # motoranpassung = self.motoranpassung(x_rotation, speed, turn)
+       #debug
+       motoranpassung = 0
        self.motors.setSpeedL(self.speedlinks + motoranpassung)
        self.motors.setSpeedR(self.speedrechts + motoranpassung)
 
