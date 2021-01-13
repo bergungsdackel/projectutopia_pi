b4a6d70b9fc9ad28cb746f0641bc76f3f94e36d2
lukasuhrig <56926393+lukasuhrig@users.noreply.github.com>
2021-01-13 12:54:38 +0100

    Update PID.py

--- PiApplication/ProjectUtopia/PID.py
+++ PiApplication/ProjectUtopia/PID.py
@@ -13,14 +13,22 @@ class PID(object):
         self.Ausgang = 0.0
         self.Regeldifferenz = 0.0
         self.Regeldifferenz_vorher = 0.0
-
+        self.maxBuffer = 100.0
+        self.maxAusgang = 30.0
+        self.regelerror = False
 
         print("PID iniziiert")
         
-    def pid(self, Eingang, Sollwert, Kp:float, Ki:float, Kd:float):
+    def pid(self, Eingang, Sollwert, Gyrokompensation:float, Kp:float, Ki:float, Kd:float):
+        kompEingang = Eingang-Gyrokompensation
         self.Regeldifferenz = Sollwert - self.Ausgang
         self.Buffer = self.Regeldifferenz * self.zeitfüreinendurchlauf + self.Buffer
-        self.Ausgang = Eingang + self.Ausgang + self.Kp * self.Regeldifferenz + self.Ki * self.Buffer  + self.Kd * ((self.Regeldifferenz - self.Regeldifferenz_vorher) / self.zeitfüreinendurchlauf)
+        self.Ausgang = kompEingang + self.Ausgang + self.Kp * self.Regeldifferenz + self.Ki * self.Buffer  + self.Kd * ((self.Regeldifferenz - self.Regeldifferenz_vorher) / self.zeitfüreinendurchlauf)
         self.Regeldifferenz_vorher = self.Regeldifferenz
         print("Ausgang = %f" % self.Ausgang)
-
+        if(Buffer > self.maxBuffer):
+            Buffer = self.maxBuffer
+        if(Buffer < -self.maxBuffer):
+            Buffer = -self.maxBuffer
+        if(abs(self.Ausgang) > abs(self.maxAusgang)):
+            self.regelerror = True
