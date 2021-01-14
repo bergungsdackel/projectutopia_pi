f9f61ed31c122b8e9bb6e4dee0e15de24adb8edc
lukasuhrig <56926393+lukasuhrig@users.noreply.github.com>
2021-01-14 08:49:57 +0100

    Update and rename collisionavoidance.py to Selfdriving.py

---
+++ PiApplication/ProjectUtopia/Selfdriving.py
@@ -0,0 +1,28 @@
+import Echo
+import pid_control
+import gyro
+
+class selfdriving(object):
+  
+  def __init__(self, motors: motorcontrol, gyro: gyro, Gyrokompensation, Kp:float, Ki:float, Kd:float,):
+    self.Echo = Echo.Echo()
+    self.motors = motors
+    self.PID_CONTROL_CLASS = pid_control.pid_control(Kp,Ki,Kd,motors)
+    
+    self.Objdetectet = False
+    self.Kp = Kp
+    self.Ki = Ki
+    self.Kd = Kd
+    
+  def detect(self):
+    if(self.Echo.Distanz < 40):
+      self.Objectdetectet = True
+    else:
+      self.Objectdetectet = False
+      
+  def drive(self):
+    self.detect()
+    if(self.Objectdetectet==False):
+      PID_CONTROL_CLASS.reglung(gyro.x_rotation, 5, 0, Gyrokompensation, self.Kp, self.Ki, self.Kd)
+    if(self.Objectdetectet==True):
+      PID_CONTROL_CLASS.reglung(gyro.x_rotation, 1, 1, Gyrokompensation, self.Kp, self.Ki, self.Kd)
