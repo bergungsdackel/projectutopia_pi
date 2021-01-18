a1d54ef52b704e8a436e839aea6659c95dc360c8
Lukas Uhrig <lukas.uhrig2000@gmail.com>
2021-01-18 12:05:50 +0100

    zeug kurz überarbeitet und hinzugefügt

--- PiApplication/ProjectUtopia/barokompass.py
+++ PiApplication/ProjectUtopia/barokompass.py
@@ -31,7 +31,7 @@ class barometer(object):
     self.cTemp = (((data[0] & 0x0F) * 65536) + (data[1] * 256) + data[2]) / 100.00
     self.pressure = (((data[3] & 0x0F) * 65536) + (data[4] * 256) + data[5]) / 100.00
     
-  def getaltitude(self)
+  def getaltitude(self):
     # HP206C address, 0x76(118)
     # Send OSR and channel setting command, 0x44(68)
     self.bus.write_byte(0x76, 0x44 | 0x01)
@@ -46,13 +46,13 @@ class barometer(object):
     # Convert the data to 20-bits
     self.altitude = (((data[0] & 0x0F) * 65536) + (data[1] * 256) + data[2]) / 100.00
   
-  def temp(self)
+  def temp(self):
     self.gettemp()
     return self.cTemp
-  def pressure(self)
+  def pressure(self):
     self.gettemp()
     return self.pressure
-  def altitude(self)
+  def altitude(self):
     self.getaltitude()
     return self.altitude
 
@@ -64,5 +64,5 @@ class kompass(object):
 
     self.hmc5883l.setContinuousMode()
     self.hmc5883l.setDeclination(2, 15)
- def kompass(self):
+def kompass(self):
     return self.hmc58831
