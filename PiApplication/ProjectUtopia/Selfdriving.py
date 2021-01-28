c19bd54bc222df4e039fbbc5c1414c1c1e12edad
Lukas <lukas.uhrig2000@gmail.com>
2021-01-28 10:24:02 +0100

    selfdriving improvement

--- PiApplication/ProjectUtopia/Selfdriving.py
+++ PiApplication/ProjectUtopia/Selfdriving.py
@@ -5,26 +5,31 @@ import motorControl
 
 class selfdriving(object):
   
-  def __init__(self, motors: motorControl, gyroClass: gyro, Gyrokompensation, EchoClass: Echo, PID_CLASS: pid_control, Kp:float, Ki:float, Kd:float):
-    self.Echo = EchoClass
-    self.motors = motors
-    self.PID_CONTROL_CLASS = PID_CLASS
+  def __init__(self, gyroClass: gyro, Gyrokompensation, EchoClass: Echo, PidClass: pid_control, Kp:float, Ki:float, Kd:float):
+    
+    #übergebene Klassen
+    self.ECHO_CLASS = EchoClass
+    self.PID_CONTROL_CLASS = PidClass
     self.GYRO_CLASS = gyroClass
-
-    self.Objdetectet = False
+    
+    #übergebene Variablen
+    self.gyrokompensation = Gyrokompensation
     self.Kp = Kp
     self.Ki = Ki
     self.Kd = Kd
-    
+
+    #neue Variablen
+    self.objdetectet = False
+
   def detect(self):
-    if(self.Echo.Distanz < 40):
-      self.Objectdetectet = True
+    if(self.ECHO_CLASS.Distanz < 40):
+      self.objectdetected = True
     else:
-      self.Objectdetectet = False
+      self.objectdetected = False
       
   def drive(self, Kp, Ki, Kd):
     self.detect()
-    if(self.Objectdetectet==False):
-      PID_CONTROL_CLASS.reglung(self.GYRO_CLASS.x_rotation, 5, 0, Gyrokompensation, Kp, Ki, Kd)
-    if(self.Objectdetectet==True):
-      PID_CONTROL_CLASS.reglung(self.GYRO_CLASS.x_rotation, 1, 1, Gyrokompensation, Kp, Ki, Kd)
+    if(self.objectdetectet==False):
+      self.PID_CONTROL_CLASS.reglung(self.GYRO_CLASS.x_rotation, 5, 0, self.gyrokompensation, Kp, Ki, Kd)
+    if(self.objectdetectet==True):
+      self.PID_CONTROL_CLASS.reglung(self.GYRO_CLASS.x_rotation, 1, 1, self.gyrokompensation, Kp, Ki, Kd)
